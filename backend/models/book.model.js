fe16732724458162329379c68e8bf61def8b97c3
TiiTro <32652455+TiiTro@users.noreply.github.com>
2021-02-20 13:45:11 +0200

    Modifying mongoose id to be a string, deleting _id and __v fields from schema

--- backend/models/book.model.js
+++ backend/models/book.model.js
@@ -5,6 +5,15 @@ const bookSchema = new mongoose.Schema({
     author: String
 });
 
+// Modifying the mongoose id to be a string, deleting _id and __v fields from schema
+bookSchema.set('toJSON', {
+    transform: (doc, returnedObject, options) => {
+        returnedObject.id = returnedObject._id;
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    }
+});
+
 const Book = mongoose.model('Book', bookSchema);
 
 module.exports = Book;
